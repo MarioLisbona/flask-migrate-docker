c5c77683e7a3ca31060291d140abfc26d1698841
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-22 14:07:53 +1100

    conditional database connections

--- app/init.py
+++ app/init.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from routes import init_routes
+import os
 
 # creates an application that is named after the name of the file
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "some_dev_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mario:password@pgsql:5432/todos"
+# Check if running on Heroku
+if 'DATABASE_URL' in os.environ:
+  app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+else:
+  # Local PostgreSQL database URL
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['LOCAL_DATABASE']
 
 # initializing routes
 init_routes(app)
