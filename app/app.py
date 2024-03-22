5629f3bd7d9b4e680aaf8f4077e7d0b05f55b7f1
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-22 13:14:45 +1100

    Move model into app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,20 @@
-from init import create_app
-app = create_app()
+from init import app, db
+from flask_migrate import Migrate
+
+# bootstrap database migrate commands
+db.init_app(app)
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = "todos"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    description = db.Column(db.String(), nullable=False)
+    due_date = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"<Todo {self.id}, {self.completed}, {self.description}>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
