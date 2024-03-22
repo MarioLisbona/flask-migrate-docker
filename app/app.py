6337dad1ccd8e72d726a91efd51fff0ba9deb225
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-22 14:43:09 +1100

    remove init.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,22 @@
-from init import app, db
 from flask_migrate import Migrate
 import os
 from flask import jsonify
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+# creates an application that is named after the name of the file
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "some_dev_key"
+# Check if running on Heroku
+if 'DATABASE_URL' in os.environ:
+  app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+else:
+  # Local PostgreSQL database URL
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['LOCAL_DATABASE']
+
+db = SQLAlchemy()
 
 # bootstrap database migrate commands
 db.init_app(app)
