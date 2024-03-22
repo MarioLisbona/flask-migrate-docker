d630b284bf5d6c19768d52ef47cc46668f00195c
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-22 13:14:54 +1100

    Update init.py

--- app/init.py
+++ app/init.py
@@ -1,17 +1,15 @@
 from flask import Flask, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
 from routes import init_routes
 
+# creates an application that is named after the name of the file
+app = Flask(__name__)
 
-def create_app(test_config=None):
+app.config["SECRET_KEY"] = "some_dev_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mario:password@pgsql:5432/todos"
 
-    # creates an application that is named after the name of the file
-    app = Flask(__name__)
+# initializing routes
+init_routes(app)
 
-    app.config["SECRET_KEY"] = "some_dev_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://usr:pwd@pgsql:5432/todos"
+db = SQLAlchemy()
 
-    # initializing routes
-    init_routes(app)
-
-    return app
