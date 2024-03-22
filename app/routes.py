554dffdba80b290d25532e936a8609dd533303ef
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-22 11:45:24 +1100

    Create home route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import jsonify
 
 def init_routes(app):
+    
+    @app.route('/')
+    def home():
+        return jsonify({
+            "msg": "Welcome"
+        })
 
     @app.route("/api", methods=["GET"])
     def get_api_base_url():
