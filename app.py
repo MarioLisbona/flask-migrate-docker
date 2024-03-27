e4c2b4842297faeed7015435786912ad083824f3
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-27 18:08:17 +1100

    Create init.py and migrate initialisation code

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 from flask_migrate import Migrate
 import os
 from flask import jsonify
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-# creates an application that is named after the name of the file
-app = Flask(__name__)
-
-app.config["SECRET_KEY"] = "some_dev_key"
-# Check if running on Heroku
-if 'DATABASE_URL' in os.environ:
-  app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL').replace(
-        'postgres://', 'postgresql+psycopg2://', 1)
-else:
-  # Local PostgreSQL database URL
-  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['LOCAL_DATABASE']
-
-db = SQLAlchemy(app)
+from init import app, db
 
 # bootstrap database migrate commands
 migrate = Migrate(app, db)
---
+++ init.py
@@ -0,0 +1,16 @@
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+import os
+
+
+
+# # creates an application that is named after the name of the file
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "some_dev_key"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL').replace(
+        'postgres://', 'postgresql+psycopg2://', 1)
+
+db = SQLAlchemy(app)
