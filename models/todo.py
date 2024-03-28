ddee97517aa687ac95fcee663a556c2be2b25069
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-28 11:47:41 +1100

    Remove todo due date

--- models/todo.py
+++ models/todo.py
@@ -6,7 +6,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     completed = db.Column(db.Boolean, nullable=False, default=False)
     description = db.Column(db.String(), nullable=False)
-    due_date = db.Column(db.DateTime, nullable=True)
+    # due_date = db.Column(db.DateTime, nullable=True)
     owner = db.Column(db.String(), nullable=True)
 
     def __repr__(self):
